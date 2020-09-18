9723fe5bbbce02584a68e163f7b86796ba21f562
WILLBER-cod <66934794+WILLBER-cod@users.noreply.github.com>
2020-09-18 11:57:54 +0800

    commit

---
+++ New Unity Project/Assets/Script/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+    [SerializeField] private float _health;
+
+    private void Update()
+    {
+        _slider.value = _health;
+    }
+
+    public void Add()
+    {
+        _health += 10;
+    }
+
+    public void Substract()
+    {
+        _health -= 10;
+    }
+}
